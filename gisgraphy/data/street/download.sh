09a2050a1f2cc34583a1c9ab9d159bfdc7c35f52
davidmasclet <davidmasclet@c16e0332-f64c-0410-aeed-4f03c014137b>
2009-03-20 22:12:45 +0000

    test zip files

--- gisgraphy/data/street/download.sh
+++ gisgraphy/data/street/download.sh
@@ -2,7 +2,10 @@
  
  paste -d: urls.txt countrycode.txt | while read line ; do echo $line| awk -F: '{print "wget "$1" -O "$2}'| sh ; done
  
+echo "Checking files....." 
+ 
 for i in *.zip
 do
-	'unzip -t ${i}'
+	echo "checking $i" 
+	`unzip -t ${i}`
 done
