f62f4731e2176fd8c0891d3be5fc772b78738563
davidmasclet <davidmasclet@c16e0332-f64c-0410-aeed-4f03c014137b>
2009-06-22 08:38:33 +0000

    update script

--- gisgraphy/data/street/4_export/export_to_csv.sh
+++ gisgraphy/data/street/4_export/export_to_csv.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
-
+us_treated=0;
 for i in *.zip
 do
 	countrycode=`echo ${i}| cut -d "." -f1`
 	echo "export $i to CSV" 
-	psql -U postgres  -d gisgraphy -h 127.0.0.1 <<COPYTEST 
+	if [[ $countrycode == 'US' && ($us_treated == 0)]]
+	then
+	        echo "countrycode is US : $countrycode"
+	        ((offset = 0));
+	        ((limit = 100000));
+	
+	        for iter  in `seq 1 290`;
+	        do
+		      echo "extraction de US $iter / 290"
+	              psql -U postgres  -d gisgraphy -h 127.0.0.1 <<COPYTEST
 \f '	' 
 \a
-\t 
-\o $countrycode.csv 
-select * from openstreetmap where countrycode='$countrycode' ;
-\q 
+\t
+\o $countrycode.$iter.csv
+select * from openstreetmap where countrycode='$countrycode' offset 1 limit 1 ;
+\q
 COPYTEST
+ 
+	                if [[ $iter == '1'  ]]
+	                then
+	                        ((offset =  $offset + 1))
+	                fi
+	                ((offset= offset + limit ));
+	        done
+us_treated=1;
+	else
+			psql -U postgres  -d gisgraphy -h 127.0.0.1 <<COPYTEST
+\f '	'
+\a
+\t
+\o $countrycode.csv
+select * from openstreetmap where countrycode='$countrycode' limit 1;
+\q
+COPYTEST
+	
+	fi
 done
-
-
-
-
