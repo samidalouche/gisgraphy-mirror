4fa1d087279af160d77e0979a0520cc27d084882
davidmasclet <davidmasclet@c16e0332-f64c-0410-aeed-4f03c014137b>
2009-07-24 14:54:03 +0000

    correct launch.sh

--- gisgraphy/src/dist/launch.sh
+++ gisgraphy/src/dist/launch.sh
@@ -2,4 +2,4 @@
 
 cd `dirname ${BASH_SOURCE[0]}`
 
-java -Dfile.encoding=UTF-8 -Xmx512m -Xms256m -jar start.jarmx512m -Xms256m -jar start.jar
+java -Dfile.encoding=UTF-8 -Xmx512m -Xms256m -jar start.jar
