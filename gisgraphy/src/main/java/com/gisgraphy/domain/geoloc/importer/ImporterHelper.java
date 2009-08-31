12707e3e64da37ee2856e562142d9bdab0b411af
davidmasclet <davidmasclet@c16e0332-f64c-0410-aeed-4f03c014137b>
2009-08-31 10:37:02 +0000

    update installation guide for centos and put the installation of postgis in linux page manage standard http code when downloading file and the two many users Code (509)

--- gisgraphy/src/main/java/com/gisgraphy/domain/geoloc/importer/ImporterHelper.java
+++ gisgraphy/src/main/java/com/gisgraphy/domain/geoloc/importer/ImporterHelper.java
@@ -34,7 +34,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Enumeration;
@@ -50,8 +49,8 @@ import com.gisgraphy.domain.geoloc.entity.Adm;
 import com.gisgraphy.helper.FeatureClassCodeHelper;
 
 /**
- * @author <a href="mailto:david.masclet@gisgraphy.com">David Masclet</a>
- *         Useful methods for importer
+ * @author <a href="mailto:david.masclet@gisgraphy.com">David Masclet</a> Useful
+ *         methods for importer
  */
 public class ImporterHelper {
 
@@ -67,27 +66,24 @@ public class ImporterHelper {
      * The regexp that every country file dump matches
      */
     public static final String GEONAMES_COUNTRY_FILE_ACCEPT_REGEX_STRING = "[A-Z][A-Z](.txt)";
-    
+
     public static final String OPENSTREETMAP_US_FILE_ACCEPT_REGEX_STRING = "(US.)[0-9]+(.txt)";
     /**
      * The regexp that every zipped country file dump matches
      */
     public static final String ZIP_FILE_ACCEPT_REGEX_STRING = ".*(.zip)";
-    
+
     public static final String TAR_BZ2_FILE_ACCEPT_REGEX_STRING = ".*(.tar.bz2)";
 
-    protected static final Logger logger = LoggerFactory
-	    .getLogger(ImporterHelper.class);
+    protected static final Logger logger = LoggerFactory.getLogger(ImporterHelper.class);
 
     public static FileFilter countryFileFilter = new FileFilter() {
 	public boolean accept(File file) {
 	    Pattern patternGeonames = Pattern.compile(GEONAMES_COUNTRY_FILE_ACCEPT_REGEX_STRING);
 	    Pattern patternOpenStreetMapUS = Pattern.compile(OPENSTREETMAP_US_FILE_ACCEPT_REGEX_STRING);
 
-	    return (file.isFile() && file.exists())
-		    && !EXCLUDED_README_FILENAME.equals(file.getName())
-		    && (patternGeonames.matcher(file.getName()).matches() || ALLCOUTRY_FILENAME
-			    .equals(file.getName()) || patternOpenStreetMapUS.matcher(file.getName()).matches());
+	    return (file.isFile() && file.exists()) && !EXCLUDED_README_FILENAME.equals(file.getName())
+		    && (patternGeonames.matcher(file.getName()).matches() || ALLCOUTRY_FILENAME.equals(file.getName()) || patternOpenStreetMapUS.matcher(file.getName()).matches());
 	}
     };
 
@@ -95,34 +91,33 @@ public class ImporterHelper {
 	public boolean accept(File file) {
 	    Pattern pattern = Pattern.compile(ZIP_FILE_ACCEPT_REGEX_STRING);
 
-	    return (file.isFile() && file.exists())
-		    && pattern.matcher(file.getName()).matches();
+	    return (file.isFile() && file.exists()) && pattern.matcher(file.getName()).matches();
 	}
     };
-    
+
     private static FileFilter tarBZ2FileFilter = new FileFilter() {
 	public boolean accept(File file) {
 	    Pattern pattern = Pattern.compile(TAR_BZ2_FILE_ACCEPT_REGEX_STRING);
 
-	    return (file.isFile() && file.exists())
-		    && pattern.matcher(file.getName()).matches();
+	    return (file.isFile() && file.exists()) && pattern.matcher(file.getName()).matches();
 	}
     };
 
     /**
      * @param directoryPath
-     *                The directory where Geonames files are
+     *            The directory where Geonames files are
      * @see #GEONAMES_COUNTRY_FILE_ACCEPT_REGEX_STRING
      * @return the allcountries.txt (@see {@linkplain #ALLCOUTRY_FILENAME} file
-     *         if present or the list of country file to Import or an empty array if there is no file
+     *         if present or the list of country file to Import or an empty
+     *         array if there is no file
      */
     public static File[] listCountryFilesToImport(String directoryPath) {
 
 	File dir = new File(directoryPath);
 
 	File[] files = dir.listFiles(countryFileFilter);
-	
-	if (files == null){
+
+	if (files == null) {
 	    return new File[0];
 	}
 
@@ -130,9 +125,7 @@ public class ImporterHelper {
 	    if (ALLCOUTRY_FILENAME.equals(file.getName())) {
 		files = new File[1];
 		files[0] = file;
-		logger
-			.info(ALLCOUTRY_FILENAME
-				+ " is present. Only this file will be imported. all other country files will be ignore");
+		logger.info(ALLCOUTRY_FILENAME + " is present. Only this file will be imported. all other country files will be ignore");
 		break;
 	    }
 	}
@@ -147,39 +140,41 @@ public class ImporterHelper {
 
     /**
      * @param directoryPath
-     *                The directory where Geonames files are to be downloaded in
-     *                order to be processed
+     *            The directory where Geonames files are to be downloaded in
+     *            order to be processed
      * @see #ZIP_FILE_ACCEPT_REGEX_STRING
-     * @return all the zip files present in the specified directory or an empty array if there is no file
+     * @return all the zip files present in the specified directory or an empty
+     *         array if there is no file
      */
     public static File[] listZipFiles(String directoryPath) {
 
 	File dir = new File(directoryPath);
 
 	File[] files = dir.listFiles(ZipFileFilter);
-	return files == null ? new File[0]: files;
+	return files == null ? new File[0] : files;
     }
-    
+
     /**
      * @param directoryPath
-     *                The directory where openstreetmap files are to be downloaded in
-     *                order to be processed
+     *            The directory where openstreetmap files are to be downloaded
+     *            in order to be processed
      * @see #TAR_BZ2_FILE_ACCEPT_REGEX_STRING
-     * @return all the zip files present in the specified directory or an empty array if there is no file
+     * @return all the zip files present in the specified directory or an empty
+     *         array if there is no file
      */
     public static File[] listTarFiles(String directoryPath) {
 
 	File dir = new File(directoryPath);
 
 	File[] files = dir.listFiles(tarBZ2FileFilter);
-	return files == null? new File[0] : files;
+	return files == null ? new File[0] : files;
     }
 
     /**
      * @param address
-     *                the address of the file to be downloaded
+     *            the address of the file to be downloaded
      * @param localFileName
-     *                the local file name (with absolute path)
+     *            the local file name (with absolute path)
      */
     public static void download(String address, String localFileName) {
 	logger.info("download file " + address + " to " + localFileName);
@@ -189,8 +184,23 @@ public class ImporterHelper {
 	try {
 	    URL url = new URL(address);
 	    out = new BufferedOutputStream(new FileOutputStream(localFileName));
-	    conn = (HttpURLConnection)url.openConnection();
-	  //  conn.getResponseCode());http://mjporg.free.fr/arcades/index.php
+	    conn = (HttpURLConnection) url.openConnection();
+	    if (conn instanceof HttpURLConnection) {
+
+		int responseCode = ((HttpURLConnection) conn).getResponseCode();
+		//manage most frequent error code and Gisgraphy specific one
+		switch (responseCode) {
+		case 509:
+		    throw new RuntimeException("Sorry, there is too many users connected, this site has limmited resources, please try again later");
+		case 500:
+		    throw new RuntimeException("Sorry, the server return an 500 status code, an internal error has occured");
+		case 404:
+		    throw new RuntimeException("Sorry, the server return an 404 status code, the file probably not exists");
+		default:
+		    break;
+		}
+		
+	    }
 	    in = conn.getInputStream();
 	    byte[] buffer = new byte[1024];
 	    int numRead;
@@ -200,15 +210,12 @@ public class ImporterHelper {
 		numWritten += numRead;
 	    }
 	    logger.info(localFileName + "\t" + numWritten);
-	}catch (UnknownHostException e) {
-	    String errorMessage = "can not download " + address + " to " + localFileName
-		    + " : " + e.getMessage()+". if the host exists and is reachable," +
-		    		" maybe this links can help : http://www.gisgraphy.com/forum/viewtopic.php?f=3&t=64 ";
+	} catch (UnknownHostException e) {
+	    String errorMessage = "can not download " + address + " to " + localFileName + " : " + e.getMessage() + ". if the host exists and is reachable," + " maybe this links can help : http://www.gisgraphy.com/forum/viewtopic.php?f=3&t=64 ";
 	    logger.warn(errorMessage);
-	    throw new GeonamesProcessorException(errorMessage,e);
+	    throw new GeonamesProcessorException(errorMessage, e);
 	} catch (Exception e) {
-	    logger.warn("can not download " + address + " to " + localFileName
-		    + " : " + e.getMessage());
+	    logger.warn("can not download " + address + " to " + localFileName + " : " + e.getMessage());
 	    throw new GeonamesProcessorException(e);
 	} finally {
 	    try {
@@ -229,7 +236,7 @@ public class ImporterHelper {
      * unzip a file in the same directory as the zipped file
      * 
      * @param file
-     *                The file to unzip
+     *            The file to unzip
      */
     public static void unzipFile(File file) {
 	logger.info("will Extracting file: " + file.getName());
@@ -251,24 +258,18 @@ public class ImporterHelper {
 		    continue;
 		}
 
-		logger.info("Extracting file: " + entry.getName() + " to "
-			+ file.getParent() + File.separator + entry.getName());
-		copyInputStream(zipFile.getInputStream(entry),
-			new BufferedOutputStream(new FileOutputStream(file
-				.getParent()
-				+ File.separator + entry.getName())));
+		logger.info("Extracting file: " + entry.getName() + " to " + file.getParent() + File.separator + entry.getName());
+		copyInputStream(zipFile.getInputStream(entry), new BufferedOutputStream(new FileOutputStream(file.getParent() + File.separator + entry.getName())));
 	    }
 
 	    zipFile.close();
 	} catch (IOException e) {
-	    logger.error("can not unzip " + file.getName() + " : "
-		    + e.getMessage());
+	    logger.error("can not unzip " + file.getName() + " : " + e.getMessage());
 	    throw new GeonamesProcessorException(e);
 	}
     }
 
-    private static final void copyInputStream(InputStream in, OutputStream out)
-	    throws IOException {
+    private static final void copyInputStream(InputStream in, OutputStream out) throws IOException {
 	byte[] buffer = new byte[1024];
 	int len;
 
@@ -282,19 +283,16 @@ public class ImporterHelper {
 
     /**
      * @param fields
-     *                the fields corresponding a split line of the csv geonames
-     *                file
+     *            the fields corresponding a split line of the csv geonames file
      * @return the modified fields whith the feature code change to
      *         ADM1,ADM2,ADM3,ADM4 according to the ADMcodes. e.g id adm1code
      *         and Adm2 code are not null : the feature code will be change to
      *         ADM2.
      */
     public static String[] virtualizeADMD(String[] fields) {
-	if (fields[7] != null && "ADMD".equals(fields[7]) && fields[6] != null
-		&& "A".equals(fields[6])) {
+	if (fields[7] != null && "ADMD".equals(fields[7]) && fields[6] != null && "A".equals(fields[6])) {
 	    // it is an ADMD, will try to detect level
-	    int level = Adm.getProcessedLevelFromCodes(fields[10], fields[11],
-		    fields[12], fields[13]);
+	    int level = Adm.getProcessedLevelFromCodes(fields[10], fields[11], fields[12], fields[13]);
 	    if (level != 0) {
 		fields[7] = "ADM" + level;
 	    }
@@ -305,7 +303,7 @@ public class ImporterHelper {
 
     /**
      * @param directoryPath
-     *                The directory to check. it can be absolute or relative
+     *            The directory to check. it can be absolute or relative
      * @return true if the path is a directory (not a file) AND exists AND is
      *         writable
      */
@@ -315,10 +313,8 @@ public class ImporterHelper {
     }
 
     public static String[] correctLastAdmCodeIfPossible(String[] fields) {
-	if (FeatureClassCodeHelper.is_Adm(fields[6], fields[7])
-		&& !AbstractGeonamesProcessor.isEmptyField(fields, 0, false)) {
-	    int level = Adm.getProcessedLevelFromFeatureClassCode(fields[6],
-		    fields[7]);
+	if (FeatureClassCodeHelper.is_Adm(fields[6], fields[7]) && !AbstractGeonamesProcessor.isEmptyField(fields, 0, false)) {
+	    int level = Adm.getProcessedLevelFromFeatureClassCode(fields[6], fields[7]);
 	    switch (level) {
 	    case 0:
 		return fields;
@@ -328,29 +324,18 @@ public class ImporterHelper {
 		}
 		return fields;
 	    case 2:
-		if (!AbstractGeonamesProcessor.isEmptyField(fields, 10, false)
-			&& AbstractGeonamesProcessor.isEmptyField(fields, 11,
-				false)) {
+		if (!AbstractGeonamesProcessor.isEmptyField(fields, 10, false) && AbstractGeonamesProcessor.isEmptyField(fields, 11, false)) {
 		    fields[11] = fields[0];// asign adm2code with featureid
 		}
 		return fields;
 	    case 3:
-		if (!AbstractGeonamesProcessor.isEmptyField(fields, 10, false)
-			&& !AbstractGeonamesProcessor.isEmptyField(fields, 11,
-				false)
-			&& AbstractGeonamesProcessor.isEmptyField(fields, 12,
-				false)) {
+		if (!AbstractGeonamesProcessor.isEmptyField(fields, 10, false) && !AbstractGeonamesProcessor.isEmptyField(fields, 11, false) && AbstractGeonamesProcessor.isEmptyField(fields, 12, false)) {
 		    fields[12] = fields[0];// asign adm3code with featureid
 		}
 		return fields;
 	    case 4:
-		if (!AbstractGeonamesProcessor.isEmptyField(fields, 10, false)
-			&& !AbstractGeonamesProcessor.isEmptyField(fields, 11,
-				false)
-			&& !AbstractGeonamesProcessor.isEmptyField(fields, 12,
-				false)
-			&& AbstractGeonamesProcessor.isEmptyField(fields, 13,
-				false)) {
+		if (!AbstractGeonamesProcessor.isEmptyField(fields, 10, false) && !AbstractGeonamesProcessor.isEmptyField(fields, 11, false) && !AbstractGeonamesProcessor.isEmptyField(fields, 12, false)
+			&& AbstractGeonamesProcessor.isEmptyField(fields, 13, false)) {
 		    fields[13] = fields[0];// asign adm4code with featureid
 		}
 		return fields;
@@ -365,14 +350,13 @@ public class ImporterHelper {
 
     /**
      * @param regexpSemiColumnSeparated
-     *                a string with multiple reqgexp separated by ';'
+     *            a string with multiple reqgexp separated by ';'
      * @return A list of {@link Pattern} or null if a regexp are not corrects
      */
     public static List<Pattern> compileRegex(String regexpSemiColumnSeparated) {
 	try {
 	    List<Pattern> patternsList = new ArrayList<Pattern>();
-	    String[] acceptRegexpString = regexpSemiColumnSeparated.trim()
-		    .split(ImporterConfig.OPTION_SEPARATOR);
+	    String[] acceptRegexpString = regexpSemiColumnSeparated.trim().split(ImporterConfig.OPTION_SEPARATOR);
 	    for (String pattern : acceptRegexpString) {
 		if (pattern != null && !pattern.trim().equals("")) {
 		    patternsList.add(Pattern.compile(pattern));
@@ -387,7 +371,7 @@ public class ImporterHelper {
 
     /**
      * @param secsIn
-     *                the number of seconds
+     *            the number of seconds
      * @return a human reading strings. example :1 hour 6 minuts 40 seconds.
      */
     public static String formatSeconds(long secsIn) {
@@ -395,12 +379,9 @@ public class ImporterHelper {
 	long hours = secsIn / 3600,
 
 	remainder = secsIn % 3600, minutes = remainder / 60, seconds = remainder % 60;
-	String displayhours = hours == 0 ? "" : hours + " hour"
-		+ getPlural(hours);
-	String displayMin = minutes == 0 ? "" : minutes + " minut"
-		+ getPlural(minutes);
-	String displaySec = seconds == 0 ? "" : seconds + " second"
-		+ getPlural(seconds);
+	String displayhours = hours == 0 ? "" : hours + " hour" + getPlural(hours);
+	String displayMin = minutes == 0 ? "" : minutes + " minut" + getPlural(minutes);
+	String displaySec = seconds == 0 ? "" : seconds + " second" + getPlural(seconds);
 	return displayhours + displayMin + displaySec;
     }
 
