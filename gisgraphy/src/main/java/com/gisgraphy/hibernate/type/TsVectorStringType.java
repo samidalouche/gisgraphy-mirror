733f8af75931964d5a08f39e4ad815201327173e
davidmasclet <davidmasclet@c16e0332-f64c-0410-aeed-4f03c014137b>
2009-10-06 21:16:44 +0000

    add an hibernate ts_vector hibernate type

---
+++ gisgraphy/src/main/java/com/gisgraphy/hibernate/type/TsVectorStringType.java
@@ -0,0 +1,140 @@
+/*******************************************************************************
+ *   Gisgraphy Project 
+ * 
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) any later version.
+ * 
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *   Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA
+ * 
+ *  Copyright 2008  Gisgraphy project 
+ *  David Masclet <davidmasclet@gisgraphy.com>
+ *  
+ *  
+ *******************************************************************************/
+package com.gisgraphy.hibernate.type;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Serializable;
+import java.io.StringReader;
+import java.sql.Clob;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.hibernate.HibernateException;
+import org.hibernate.usertype.UserType;
+
+/**
+ * Hibernatetype to map postgresql ts_vector to String
+ * @author <a href="mailto:david.masclet@gisgraphy.com">David Masclet</a>
+ *
+ */
+public class TsVectorStringType implements UserType{
+	
+	  private static final int READ_BUFFER_SIZE = 4096;
+
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.usertype.UserType#assemble(java.io.Serializable, java.lang.Object)
+	 */
+	public Object assemble(Serializable cached, Object owner) throws HibernateException {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.usertype.UserType#deepCopy(java.lang.Object)
+	 */
+	public Object deepCopy(Object value) throws HibernateException {
+		if (value == null) {
+		    return null;
+		}
+		return new String((String) value);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.usertype.UserType#disassemble(java.lang.Object)
+	 */
+	public Serializable disassemble(Object arg0) throws HibernateException {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.usertype.UserType#equals(java.lang.Object, java.lang.Object)
+	 */
+	public boolean equals(Object x, Object y) throws HibernateException {
+		return (x == y) || ((x != null) && (y != null) && (x.equals(y)));
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.usertype.UserType#hashCode(java.lang.Object)
+	 */
+	public int hashCode(Object obj) throws HibernateException {
+		return obj.hashCode();
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.usertype.UserType#isMutable()
+	 */
+	public boolean isMutable() {
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.usertype.UserType#nullSafeGet(java.sql.ResultSet, java.lang.String[], java.lang.Object)
+	 */
+	public Object nullSafeGet(ResultSet rs, String[] names, Object owner) throws HibernateException, SQLException {
+		
+		return rs.getObject(names[0]);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.usertype.UserType#nullSafeSet(java.sql.PreparedStatement, java.lang.Object, int)
+	 */
+	public void nullSafeSet(PreparedStatement st, Object value, int index) throws HibernateException, SQLException {
+		if (value != null) {
+				if (!(value instanceof String)){
+					throw new HibernateException("object must be a string to be converted");
+				}
+		    st.setString(index,"to_tsvector('"+((String) value)+"')");
+		} else {
+		    st.setNull(index, sqlTypes()[0]);
+		}
+
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.usertype.UserType#replace(java.lang.Object, java.lang.Object, java.lang.Object)
+	 */
+	public Object replace(Object original, Object target, Object owner) throws HibernateException {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.usertype.UserType#returnedClass()
+	 */
+	public Class returnedClass() {
+		return String.class;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.usertype.UserType#sqlTypes()
+	 */
+	public int[] sqlTypes() {
+		return new int[] { Types.STRUCT };
+	}
+
+}
