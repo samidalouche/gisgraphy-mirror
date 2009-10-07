8ad9c59c6cef6feb53247a1d4855a0ea3ecf48f8
davidmasclet <davidmasclet@c16e0332-f64c-0410-aeed-4f03c014137b>
2009-10-07 08:38:08 +0000

    add a method to process string for fulltext indexation

---
+++ gisgraphy/src/test/java/com/gisgraphy/helper/StringHelperTest.java
@@ -0,0 +1,15 @@
+package com.gisgraphy.helper;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+
+    @Test
+    public void testTransformStringForFulltextIndexation() {
+	Assert.assertEquals("letter without accent should not be modified","  e e e e je me souviens de ce zouave qui jouait du xylophone en buvant du whisky",
+		StringHelper.TransformStringForFulltextIndexation("- \u00E8 \u00E9 \u00EA \u00EB JE ME SOUVIENS de ce zouave qui jouait du-xylophone en buvant.du whisky"));
+    }
+
+}
