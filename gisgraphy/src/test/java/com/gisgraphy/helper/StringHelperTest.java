2cc23cae982a9428488ed6b9deec910009068835
davidmasclet <davidmasclet@c16e0332-f64c-0410-aeed-4f03c014137b>
2009-10-07 08:38:40 +0000

    improve test

--- gisgraphy/src/test/java/com/gisgraphy/helper/StringHelperTest.java
+++ gisgraphy/src/test/java/com/gisgraphy/helper/StringHelperTest.java
@@ -8,8 +8,8 @@ public class StringHelperTest {
 
     @Test
     public void testTransformStringForFulltextIndexation() {
-	Assert.assertEquals("letter without accent should not be modified","  e e e e je me souviens de ce zouave qui jouait du xylophone en buvant du whisky",
-		StringHelper.TransformStringForFulltextIndexation("- \u00E8 \u00E9 \u00EA \u00EB JE ME SOUVIENS de ce zouave qui jouait du-xylophone en buvant.du whisky"));
+	Assert.assertEquals("letter without accent should not be modified"," e e e e e je me souviens de ce zouave qui jouait du xylophone en buvant du whisky",
+		StringHelper.TransformStringForFulltextIndexation("-é \u00E8 \u00E9 \u00EA \u00EB JE ME SOUVIENS de ce zouave qui jouait du-xylophone en buvant.du whisky"));
     }
 
 }
