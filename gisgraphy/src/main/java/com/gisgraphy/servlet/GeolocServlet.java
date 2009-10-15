66d7a392d5f3b0a68b37506c5c064c252cd8de0a
davidmasclet <davidmasclet@c16e0332-f64c-0410-aeed-4f03c014137b>
2009-10-15 21:17:35 +0000

    improve logs

--- gisgraphy/src/main/java/com/gisgraphy/servlet/GeolocServlet.java
+++ gisgraphy/src/main/java/com/gisgraphy/servlet/GeolocServlet.java
@@ -129,7 +129,7 @@ public class GeolocServlet extends GisgraphyServlet {
 	    geolocSearchEngine.executeAndSerialize(query, resp
 		    .getOutputStream());
 	} catch (RuntimeException e) {
-	    logger.error("error while creating geoloc query : " + e);
+	    logger.error("error while execute a geoloc query from http request : " + e);
 	    e.printStackTrace();
 	    String errorMessage = this.debugMode ? " : " + e.getMessage() : "";
 	    sendCustomError(ResourceBundle
