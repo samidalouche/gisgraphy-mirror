e04d8d5f0b00050904c91c863eb75160c8e71a9b
davidmasclet <davidmasclet@c16e0332-f64c-0410-aeed-4f03c014137b>
2009-10-15 21:18:16 +0000

    improve logs

--- gisgraphy/src/main/java/com/gisgraphy/servlet/FulltextServlet.java
+++ gisgraphy/src/main/java/com/gisgraphy/servlet/FulltextServlet.java
@@ -131,7 +131,7 @@ public class FulltextServlet extends GisgraphyServlet {
 	    fullTextSearchEngine.executeAndSerialize(query, resp
 		    .getOutputStream());
 	} catch (RuntimeException e) {
-	    logger.error("error while creating full text query : " + e);
+	    logger.error("error while execute a fulltext query from http request : " + e);
 	    e.printStackTrace();
 	    String errorMessage = this.debugMode ? " : " + e.getMessage() : "";
 	    sendCustomError(ResourceBundle
