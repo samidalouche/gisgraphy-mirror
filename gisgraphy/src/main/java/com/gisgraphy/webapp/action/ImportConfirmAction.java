2992cd8af57f406f03373fc1a1b3f589fc5e8e89
David Masclet <davidmasclet@gmail.com>
2009-11-12 21:10:52 +0000

    start importwizard

--- gisgraphy/src/main/java/com/gisgraphy/webapp/action/ImportConfirmAction.java
+++ gisgraphy/src/main/java/com/gisgraphy/webapp/action/ImportConfirmAction.java
@@ -44,156 +44,170 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 public class ImportConfirmAction extends ActionSupport {
 
-    /**
-     * Default serial ID
-     */
-    private static final long serialVersionUID = 2387732133217655558L;
+	/**
+	 * Default serial ID
+	 */
+	private static final long serialVersionUID = 2387732133217655558L;
 
-    private ImporterConfig importerConfig;
+	private ImporterConfig importerConfig;
 
-    private IImporterManager importerManager;
+	private IImporterManager importerManager;
 
-    public static String STATUS = "status";
+	public static final String STATUS = "status";
 
-    private IFullTextSearchEngine fullTextSearchEngine;
+	public static final String STEP_BASE_VIEW_NAME = "importWizardStep";
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.opensymphony.xwork2.ActionSupport#execute()
-     */
-    @Override
-    public String execute() throws Exception {
-	if (importerManager.isInProgress() || importerManager.isAlreadyDone()) {
-	    return STATUS;
+	private IFullTextSearchEngine fullTextSearchEngine;
+
+	public int step_number;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.opensymphony.xwork2.ActionSupport#execute()
+	 */
+	@Override
+	public String execute() throws Exception {
+		if (importerManager.isInProgress() || importerManager.isAlreadyDone()) {
+			return STATUS;
+		}
+		return super.execute();
 	}
-	return super.execute();
-    }
 
-    /**
-     * @return the importerConfig
-     */
-    public ImporterConfig getImporterConfig() {
-	return importerConfig;
-    }
+	public String step1() {
+		if (importerManager.isInProgress() || importerManager.isAlreadyDone()) {
+			return STATUS;
+		}
+		this.step_number = 1;
+		return STEP_BASE_VIEW_NAME + step_number;
+	}
+	
+	public String step2() {
+		if (importerManager.isInProgress() || importerManager.isAlreadyDone()) {
+			return STATUS;
+		}
+		this.step_number = 2;
+		return STEP_BASE_VIEW_NAME + step_number;
+	}
 
-    /**
-     * @param importerConfig
-     *                the importerConfig to set
-     */
-    public void setImporterConfig(ImporterConfig importerConfig) {
-	this.importerConfig = importerConfig;
-    }
+	/**
+	 * @return the importerConfig
+	 */
+	public ImporterConfig getImporterConfig() {
+		return importerConfig;
+	}
 
-    /**
-     * @return true if the directory with the file to import exists and is
-     *         accessible
-     */
-    public boolean isDownloadDirectoryAccessible() {
-	return ImporterHelper.isDirectoryAccessible(importerConfig
-		.getGeonamesDir());
-    }
-    
-    /**
-     * @return true if the directory with the file to import exists and is
-     *         accessible
-     */
-    public boolean isOpenStreetMapDownloadDirectoryAccessible() {
-	return ImporterHelper.isDirectoryAccessible(importerConfig
-		.getOpenStreetMapDir());
-    }
+	/**
+	 * @return true if the directory with the file to import exists and is
+	 *         accessible
+	 */
+	public boolean isDownloadDirectoryAccessible() {
+		return ImporterHelper.isDirectoryAccessible(importerConfig.getGeonamesDir());
+	}
 
-    /**
-     * @return true if the regexp of the feature class/ code are correct
-     */
-    public boolean isRegexpCorrects() {
-	return ImporterHelper.compileRegex(importerConfig
-		.getAcceptRegExString()) != null;
-    }
+	/**
+	 * @return true if the directory with the file to import exists and is
+	 *         accessible
+	 */
+	public boolean isOpenStreetMapDownloadDirectoryAccessible() {
+		return ImporterHelper.isDirectoryAccessible(importerConfig.getOpenStreetMapDir());
+	}
 
-    /**
-     * @return true if he fulltext search engine is alive
-     */
-    public boolean isFulltextSearchEngineAlive() {
-	return fullTextSearchEngine.isAlive();
-    }
+	/**
+	 * @return true if the regexp of the feature class/ code are correct
+	 */
+	public boolean isRegexpCorrects() {
+		return ImporterHelper.compileRegex(importerConfig.getAcceptRegExString()) != null;
+	}
 
-    /**
-     * @return true if he fulltext search engine is alive
-     */
-    public String getFulltextSearchEngineURL() {
-	return fullTextSearchEngine.getURL();
-    }
-    
-    /**
-     * @return true if he Geonames importer is enabled
-     */
-    public boolean isGeonamesImporterEnabled() {
-	return importerConfig.isGeonamesImporterEnabled();
-    }
-    
-   
-    /**
-     * Disable Geonames importer
-     */
-    public String disableGeonamesImporter() {
-	importerConfig.setGeonamesImporterEnabled(false);
-	return Action.SUCCESS;
-    }
-    
-    /**
-     * Enable Geonames importer
-     */
-    public String enableGeonamesImporter() {
-	importerConfig.setGeonamesImporterEnabled(true);
-	return Action.SUCCESS;
-    }
-    
-    /**
-     * @return true if he openStreetMap importer is enabled
-     */
-    public boolean isOpenStreetMapImporterEnabled() {
-	return importerConfig.isOpenstreetmapImporterEnabled();
-    }
-    
-   
-    /**
-     * Disable OpenStreetMap importer
-     */
-    public String disableOpenStreetMapImporter() {
-	importerConfig.setOpenstreetmapImporterEnabled(false);
-	return Action.SUCCESS;
-    }
-    
-    /**
-     * Enable OpenStreetMap importer
-     */
-    public String enableOpenStreetMapImporter() {
-	importerConfig.setOpenstreetmapImporterEnabled(true);
-	return Action.SUCCESS;
-    }
+	/**
+	 * @return true if he fulltext search engine is alive
+	 */
+	public boolean isFulltextSearchEngineAlive() {
+		return fullTextSearchEngine.isAlive();
+	}
 
-    /**
-     * @param importerManager
-     *                the importerManager to set
-     */
-    @Required
-    public void setImporterManager(IImporterManager importerManager) {
-	this.importerManager = importerManager;
-    }
+	/**
+	 * @return true if he fulltext search engine is alive
+	 */
+	public String getFulltextSearchEngineURL() {
+		return fullTextSearchEngine.getURL();
+	}
 
-    /**
-     * @param fullTextSearchEngine
-     *                the fullTextSearchEngine to set
-     */
-    @Required
-    public void setFullTextSearchEngine(
-	    IFullTextSearchEngine fullTextSearchEngine) {
-	this.fullTextSearchEngine = fullTextSearchEngine;
-    }
-    
-    public Map<String,String> getConfigValuesMap(){
-	return PropertiesHelper.convertBundleToMap(ResourceBundle.getBundle("env"));
-     }
+	/**
+	 * @return true if he Geonames importer is enabled
+	 */
+	public boolean isGeonamesImporterEnabled() {
+		return importerConfig.isGeonamesImporterEnabled();
+	}
+
+	/**
+	 * Disable Geonames importer
+	 */
+	public String disableGeonamesImporter() {
+		importerConfig.setGeonamesImporterEnabled(false);
+		return Action.SUCCESS;
+	}
+
+	/**
+	 * Enable Geonames importer
+	 */
+	public String enableGeonamesImporter() {
+		importerConfig.setGeonamesImporterEnabled(true);
+		return Action.SUCCESS;
+	}
+
+	/**
+	 * @return true if he openStreetMap importer is enabled
+	 */
+	public boolean isOpenStreetMapImporterEnabled() {
+		return importerConfig.isOpenstreetmapImporterEnabled();
+	}
+
+	/**
+	 * Disable OpenStreetMap importer
+	 */
+	public String disableOpenStreetMapImporter() {
+		importerConfig.setOpenstreetmapImporterEnabled(false);
+		return Action.SUCCESS;
+	}
+
+	/**
+	 * Enable OpenStreetMap importer
+	 */
+	public String enableOpenStreetMapImporter() {
+		importerConfig.setOpenstreetmapImporterEnabled(true);
+		return Action.SUCCESS;
+	}
+
+	/**
+	 * @param importerManager
+	 *                the importerManager to set
+	 */
+	@Required
+	public void setImporterManager(IImporterManager importerManager) {
+		this.importerManager = importerManager;
+	}
+
+	/**
+	 * @param fullTextSearchEngine
+	 *                the fullTextSearchEngine to set
+	 */
+	@Required
+	public void setFullTextSearchEngine(IFullTextSearchEngine fullTextSearchEngine) {
+		this.fullTextSearchEngine = fullTextSearchEngine;
+	}
+
+	public Map<String, String> getConfigValuesMap() {
+		return PropertiesHelper.convertBundleToMap(ResourceBundle.getBundle("env"));
+	}
+
+	/**
+	 * @param importerConfig
+	 *                the importerConfig to set
+	 */
+	public void setImporterConfig(ImporterConfig importerConfig) {
+		this.importerConfig = importerConfig;
+	}
 
 }
