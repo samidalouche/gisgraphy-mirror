36b8ff4acf7fd54a161cd255cf589c72225b14d1
David Masclet <davidmasclet@gmail.com>
2009-12-08 09:23:22 +0000

    update doc for untar class

--- gisgraphy/src/main/java/com/gisgraphy/helper/Untar.java
+++ gisgraphy/src/main/java/com/gisgraphy/helper/Untar.java
@@ -37,68 +37,60 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Utility class to untar files, file can be gziped or Bzip2
+ * Utility class to untar files, files can be zipped in multi format (extension
+ * tar, tar.gzip,tar.gz, tar.bz2, tar.bzip2 are supported).
  * 
- *  @author <a href="mailto:david.masclet@gisgraphy.com">David Masclet</a>
+ * @author <a href="mailto:david.masclet@gisgraphy.com">David Masclet</a>
  * 
  */
 public class Untar {
     private String tarFileName;
     private File dest;
-    
+
     /**
      * The logger
      */
-    private static final Logger logger = LoggerFactory
-	    .getLogger(Untar.class);
+    private static final Logger logger = LoggerFactory.getLogger(Untar.class);
 
-  
     /**
-     * @param tarFileName the path to the file we want to untar
-     * @param dest the pat where the file should be untar
+     * @param tarFileName
+     *            the path to the file we want to untar
+     * @param dest
+     *            the path where the file should be untar
      */
     public Untar(String tarFileName, File dest) {
 	this.tarFileName = tarFileName;
 	this.dest = dest;
     }
 
-   /* public static void main(String args[]) {
-	try {
-	    Untar untar = new Untar("/home/dmasclet/street/done/csv/files.tar.bz2", new File("/home/dmasclet/decompress"));
-	    untar.untar();
-	} catch (Exception e) {
-	}
-
-    }*/
-
     private InputStream getDecompressedInputStream(final String name, final InputStream istream) throws IOException {
-	logger.info("untar: decompress "+name+" to "+ dest);
+	logger.info("untar: decompress " + name + " to " + dest);
 	if (name == null) {
 	    throw new RuntimeException("fileName to decompress can not be null");
 	}
-	if (name.toLowerCase().endsWith("gzip")|| name.toLowerCase().endsWith("gz")) {
+	if (name.toLowerCase().endsWith("gzip") || name.toLowerCase().endsWith("gz")) {
 	    return new BufferedInputStream(new GZIPInputStream(istream));
-	} else  if (name.toLowerCase().endsWith("bz2") || name.toLowerCase().endsWith("bzip2")) {
-		final char[] magic = new char[] { 'B', 'Z' };
-		for (int i = 0; i < magic.length; i++) {
-		    if (istream.read() != magic[i]) {
-			throw new RuntimeException("Invalid bz2 file." + name);
-		    }
+	} else if (name.toLowerCase().endsWith("bz2") || name.toLowerCase().endsWith("bzip2")) {
+	    final char[] magic = new char[] { 'B', 'Z' };
+	    for (int i = 0; i < magic.length; i++) {
+		if (istream.read() != magic[i]) {
+		    throw new RuntimeException("Invalid bz2 file." + name);
 		}
-		return new BufferedInputStream(new CBZip2InputStream(istream));
 	    }
-	else if (name.toLowerCase().endsWith("tar")){
+	    return new BufferedInputStream(new CBZip2InputStream(istream));
+	} else if (name.toLowerCase().endsWith("tar")) {
 	    return istream;
 	}
 	throw new RuntimeException("can only detect compression for extension tar, gzip, gz, bz2, or bzip2");
     }
 
     /**
-     * process the untar
+     * process the untar operation
+     * 
      * @throws IOException
      */
     public void untar() throws IOException {
-	logger.info("untar: untar "+tarFileName+" to "+ dest);
+	logger.info("untar: untar " + tarFileName + " to " + dest);
 	TarInputStream tin = null;
 	try {
 	    if (!dest.exists()) {
@@ -111,11 +103,11 @@ public class Untar {
 
 	    while (tarEntry != null) {
 		File destPath = new File(dest.toString() + File.separatorChar + tarEntry.getName());
-		
+
 		if (tarEntry.isDirectory()) {
 		    destPath.mkdir();
 		} else {
-		    logger.info("untar: untar "+tarEntry.getName()+" to "+ destPath);
+		    logger.info("untar: untar " + tarEntry.getName() + " to " + destPath);
 		    FileOutputStream fout = new FileOutputStream(destPath);
 		    try {
 			tin.copyEntryContents(fout);
