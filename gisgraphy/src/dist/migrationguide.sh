4b6b4057c369ff175d045a41fe1ac1b97a5d420c
David Masclet <davidmasclet@gmail.com>
2010-01-05 14:33:27 +0000

    update migration guide

--- gisgraphy/src/dist/migrationguide.sh
+++ gisgraphy/src/dist/migrationguide.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 (( from = 1));
 ((batch_size = 1000));
 ((to = from + batch_size))
@@ -7,5 +9,11 @@
 			((to = from + batch_size -1))
 			((percent = $iter *100 / 28700))
 			echo "$percent % done (iter=$iter) ";
-		      `psql -hwww.gisgraphy.com -Upostgres -dgisgraphy3 -c "UPDATE openstreetmap set  textsearchvector=to_tsvector('simple',coalesce(name,'')) where name is not null and gid >= $from and gid < $to  " 2>&1 >/dev/null`
+		      `psql -hwww.gisgraphy.com -Upostgres -dgisgraphy -c "UPDATE openstreetmap set  textsearchvector=to_tsvector('simple',coalesce(name,'')) where name is not null and gid >= $from and gid < $to  " 2>&1 >/dev/null`
 done
+
+echo "will create openstreetmap fulltext search index";
+psql -hlocalhost -Upostgres -dgisgraphy -c "CREATE INDEX textsearchvectorindexopenstreetmap ON openstreetmap USING gin(textsearchvector)" 2>&1 >/dev/null
+
+echo "will create openstreetmap shape index";
+psql -hlocalhost -Upostgres -dgisgraphy -c "CREATE INDEX shapeindexopenstreetmap ON openstreetmap USING GIST (shape)" 2>&1 >/dev/null
