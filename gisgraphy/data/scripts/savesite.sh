c55a393fbe7264f38d183c25cdfc87396f54c18f
David Masclet <davidmasclet@gmail.com>
2010-01-26 08:34:54 +0000

    update save script

--- gisgraphy/data/scripts/savesite.sh
+++ gisgraphy/data/scripts/savesite.sh
@@ -1,10 +1,12 @@
  #!/bin/bash
  
+ export SITE_HOME="/home/dmasclet/dirtosave/"
+ export SAVE_SITE_HOME="/home/dmasclet/dirbackup/"
  dataBkUp="/data/"
  FilesToSave[0]="abuse.txt";
  FileToSave[1]=""
  
- check_vars{
+function  check_vars {
  if [[ -z $SITE_HOME ]]
  then
  echo "please set the SITE_HOME directory first (must ends with a slash) : "
@@ -17,24 +19,39 @@
  then
  echo "please set the SAVE_SITE_HOME directory first : "
  read save_site_home
- export SAVE_SITE_HOME="$save_site_home" 
+ export SAVE_SITE_HOME="$save_site_home"
  echo "SAVE_SITE_HOME directory has been set to $SAVE_SITE_HOME"
  fi
+ 
+ 
+ if [[ ! -d $SITE_HOME ]]
+ then
+ echo "$SITE_HOME does not exist"
+ exit 1
+ fi
+ 
+ 
+ if [[ ! -d $SAVE_SITE_HOME ]]
+ then
+ echo "$SAVE_SITE_HOME does not exist"
+ exit 1 
+ fi
+ 
+ 
  }
  
 function save_site_data {
-	`mkdir $SAVE_SITE_HOME$dataBkUp`
+	#`mkdir $SAVE_SITE_HOME$dataBkUp`
+	echo "will save $SITE_HOME to $SAVE_SITE_HOME"
 	date=`date "+%Y-%m-%dT%H:%M:%S"`
 	#ending slash of $SITE_HOME is important
-	rsync -aP --delete --link-dest=$SAVE_SITE_HOME/Backups/current SITE_HOME/ $SAVE_SITE_HOME/Backups/back-$date
-	rm -f $HOME/Backups/current
-	ln -s $SAVE_SITE_HOME/Backups/back-$date $SAVE_SITE_HOME/Backups/current
+	rsync -aP --delete --link-dest=$SAVE_SITE_HOME/current $SITE_HOME/ $SAVE_SITE_HOME/backup-$date
+	rm -f $SAVE_SITE_HOME/current
+	ln -s $SAVE_SITE_HOME/backup-$date $SAVE_SITE_HOME/current
 }
 
 function dump_site_data {
 echo "dump_site_data is not implemented yet "
 }
 
- check_vars
- save_site_data
- dump_site_data
+check_vars && save_site_data && dump_site_data
