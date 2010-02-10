687606b88f2413f77cfb8d97f3c3462361579c55
David Masclet <davidmasclet@gmail.com>
2010-02-10 16:23:32 +0000

    add some bench and script to generate data

--- gisgraphy/src/bench/data/generateSampleData.sh
+++ gisgraphy/src/bench/data/generateSampleData.sh
@@ -6,7 +6,7 @@ databaseName="gisgraphy"
 tableName="osm"
 pgUser="postgres"
 pgHost="127.0.0.1"
-dataset_size="1000"
+dataset_size="30000"
 
 function psql_exportToCSV {
 psql -U $pgUser  -d$databaseName -h$pgHost <<COPYTEST
@@ -20,8 +20,8 @@ COPYTEST
 }
 
 
-psql_exportToCSV "select replace(replace(astext(location), 'POINT(',''),')','') from gisfeature g where location is not null limit $dataset_size ;" "features_location.csv"
-psql_exportToCSV "select featureid from gisfeature g where location is not null limit $dataset_size ;" "featureid.csv"
-psql_exportToCSV "select name from gisfeature g where location is not null limit $dataset_size ;" "features_name.csv"
-psql_exportToCSV "select replace(replace(astext(location), 'POINT(',''),')','') from openstreetmap g where location is not null limit $dataset_size ;" "streets_location.csv"
+psql_exportToCSV "select replace(replace(astext(location), 'POINT(',''),')','') from city c where location is not null limit $dataset_size ;" "features_location.csv"
+psql_exportToCSV "select featureid from city g where location is not null limit $dataset_size ;" "featureid.csv"
+psql_exportToCSV "select replace(name,' ','%20') from city c where name  is not null limit $dataset_size ;" "features_name.csv"
+psql_exportToCSV "select replace(replace(astext(location), 'POINT(',''),')','') from openstreetmap o where location is not null limit $dataset_size ;" "streets_location.csv"
 
