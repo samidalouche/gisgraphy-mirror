98956742b90fba0b7e27c7721c077263c8b7b2b0
David Masclet <davidmasclet@gmail.com>
2010-07-28 20:25:04 +0000

    rename some folder update readme for download site to tell to run sql index files

--- gisgraphy/site.sh
+++ gisgraphy/site.sh
@@ -7,3 +7,6 @@ mvn clean site -Dmaven.test.skip -Dresetdb=false -fae -Ddependency.locations.ena
 mv target/site/index.html target/site/index_old.html
 grep "end header" target/site/index_old.html -B 9999 > target/site/include/header.inc
 grep "start footer" target/site/index_old.html -A 9999 > target/site/include/footer.inc
+sed 's/gisgraphy - About/Free Open source Geocoder via REST webservices (for geonames and openstreetmap data)/g' target/site/include/header.inc > header.inc
+mv header.inc target/site/include/header.inc
+rm _rf target/site/index_old.html
