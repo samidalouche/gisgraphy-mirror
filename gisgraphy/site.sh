14b252721f15efcdf6a9009ed11d0ae6745432af
David Masclet <davidmasclet@gmail.com>
2010-08-03 14:21:42 +0000

    change 34 million to 42 million reactivate tests to generate surefire reports static/* ressource are now serve by the static filter

--- gisgraphy/site.sh
+++ gisgraphy/site.sh
@@ -3,7 +3,7 @@
 cd src/skin
 mvn clean install
 cd ../../
-mvn clean site -Dmaven.test.skip -Dresetdb=false -fae -Ddependency.locations.enabled=false
+mvn clean site  -Dresetdb=false -fae -Ddependency.locations.enabled=false
 mv target/site/index.html target/site/index_old.html
 grep "end header" target/site/index_old.html -B 9999 > target/site/include/header.inc
 grep "start footer" target/site/index_old.html -A 9999 > target/site/include/footer.inc
