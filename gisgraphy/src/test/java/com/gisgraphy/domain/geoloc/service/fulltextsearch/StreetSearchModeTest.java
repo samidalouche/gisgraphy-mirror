3e566ee87e82a45dba634500bfb34b3bca67d0bc
David Masclet <davidmasclet@gmail.com>
2011-01-22 14:06:06 +0000

    add test for streetsearchmode

--- gisgraphy/src/test/java/com/gisgraphy/domain/geoloc/service/fulltextsearch/StreetSearchModeTest.java
+++ gisgraphy/src/test/java/com/gisgraphy/domain/geoloc/service/fulltextsearch/StreetSearchModeTest.java
@@ -11,5 +11,12 @@ public class StreetSearchModeTest {
 	public void getDefaultShouldReturnCorrectValue(){
 		Assert.assertEquals("the default street search mode is not the expected one",StreetSearchMode.FULLTEXT, StreetSearchMode.getDefault());
 	}
+	
+	@Test
+	public void geFromString(){
+		Assert.assertEquals("the default street search mode must be return if searchmode can not be determined ",StreetSearchMode.getDefault(), StreetSearchMode.getFromString("foo"));
+		Assert.assertEquals("getFromString ShouldBe case insensitive",StreetSearchMode.CONTAINS, StreetSearchMode.getFromString("conTaIns"));
+		Assert.assertEquals("getFromString ShouldBe case insensitive",StreetSearchMode.FULLTEXT, StreetSearchMode.getFromString("fulltext"));
+	}
 
 }
