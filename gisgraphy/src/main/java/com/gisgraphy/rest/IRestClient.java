5ba4a5029782780228bbd2882693ecea94be4082
David Masclet <davidmasclet@gmail.com>
2011-01-28 21:13:25 +0000

    remove restclient class because it is extracted in gisgraphy-utils lib

--- gisgraphy/src/main/java/com/gisgraphy/rest/IRestClient.java
+++
@@ -1,9 +0,0 @@
-package com.gisgraphy.rest;
-
-import com.gisgraphy.serializer.OutputFormat;
-
-public interface IRestClient {
-
-    public <T> T get(String url, Class<T> classToBeBound, OutputFormat format) throws RestClientException;
-
-}
