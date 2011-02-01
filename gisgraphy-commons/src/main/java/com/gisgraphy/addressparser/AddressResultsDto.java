df71c9524bed882275305d8a3ae1b51f4e94e5a2
David Masclet <davidmasclet@gmail.com>
2011-02-01 13:10:28 +0000

    add some domain objects

--- gisgraphy-commons/src/main/java/com/gisgraphy/addressparser/AddressResultsDto.java
+++ gisgraphy-commons/src/main/java/com/gisgraphy/addressparser/AddressResultsDto.java
@@ -27,6 +27,7 @@ package com.gisgraphy.addressparser;
 
 import java.util.List;
 
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
--- gisgraphy-commons/src/main/java/com/gisgraphy/addressparser/Constants.java
+++ gisgraphy-commons/src/main/java/com/gisgraphy/addressparser/Constants.java
@@ -5,5 +5,7 @@ public class Constants {
 	public final static String ADDRESS_ROOT_JAXB_NAME= "Address";
 	
 	public final static String ADDRESS_RESULT_ROOT_JAXB_NAME= "results";
+	
+	public final static String BUNDLE_KEY= "addressparser.properties";
 
 }
